e9610633d5d0236441501d9529af26ee2e6bd547
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-01 10:20:50 -0400

    Changed Comments to done

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,9 +5,9 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //Done
 
-        //TODO Make the property, "Day", type enum
+        //Done
         public enum Day {monday,tuesday,wednesday,thursday,friday,saturdasy,sunday  };
 
     }
